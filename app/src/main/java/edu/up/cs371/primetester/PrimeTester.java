9b56bec3052cc3a1db024d6464ebc3d559c6b160
camachon18 <camachon18@up.edu>
2015-09-02 16:46:12 -0700

    Fixed isPrime method and indentation in PrimeTesterActivity

--- app/src/main/java/edu/up/cs371/primetester/PrimeTester.java
+++ app/src/main/java/edu/up/cs371/primetester/PrimeTester.java
@@ -15,6 +15,26 @@ public class PrimeTester {
      */
     public static boolean isPrime(long n) {
         // for now, return a random result
-        return Math.random() > 0.5;
+        if (n == 0 || n == 1) {
+
+            return false;
+        }
+        if (n == 2) {
+            return true;
+        }
+
+        if (n != 2 && n != 1) {
+
+            for(int i = 2; i < n; i++){
+                if (n % i == 0) {
+                    return false;
+                }
+                else {
+                    return true;
+                }
+            }
+        }
+
+        return true;
     }
 }
